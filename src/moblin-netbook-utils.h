b2ec4dcbb0cbaca3824e7db45a11ed1ab874f349
Thomas Wood <thomas@linux.intel.com>
2009-01-15 10:35:20 +0000

    Update the license headers with Intel approved wording

--- src/moblin-netbook-utils.h
+++ src/moblin-netbook-utils.h
@@ -1,25 +1,22 @@
 /*
- * moblin_netbook-utils.c
- * This file is part of moblin_netbook-engine
+ * moblin-gtk-engine - A GTK+ theme engine for Moblin
  *
- * Copyright (C) 2006,2007 - OpenedHand Ltd
+ * Copyright (c) 2008, Intel Corporation.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
-
 
 #include <gtk/gtk.h>
 
